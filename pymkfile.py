76c5bf1180f2d7403acd9aee29057c3543004bb7
khoanguyen8496 <khoanguyen8496@gmail.com>
2018-04-16 15:10:40 +0700

    fix makefile generator for numpy

--- pymkfile.py
+++ pymkfile.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python2
 import distutils.sysconfig
 import string, sys
+import glob
+import re
+source_re = re.compile(r'(.+?)\.c', re.IGNORECASE);
 configopts = {}
 maketemplate = """
 PYLIB=%(pythonlib)s
 PYINC=-I%(pythoninc)s
 LIBS=%(pylibs)s
 OPTS=%(pyopt)s
+DEPS=%(deps)s
 PROGRAMS=%(programs)s
-all: $(PROGRAMS)
+all: $(PROGRAMS) $(DEPS)
 """
 configopts['pythonlib'] = distutils.sysconfig.get_config_var('LIBPL') \
         + '/' + \
         distutils.sysconfig.get_config_var('LIBRARY')
 configopts['pythoninc'] = ''
-configopts['pylibs'] = ''
+configopts['pylibs'] = distutils.sysconfig.get_config_var('LINKFORSHARED') + ' '
+# configopts['pylibs'] = ''
 for dir in string.split(distutils.sysconfig.get_config_var('INCLDIRSTOMAKE')):
     configopts['pythoninc'] += '-I%s ' % (dir,)
 for dir in string.split(distutils.sysconfig.get_config_var('LIBDIR')):
@@ -25,14 +30,19 @@ configopts['pylibs'] += distutils.sysconfig.get_config_var('MODLIBS') \
         + ' ' + \
         distutils.sysconfig.get_config_var('SYSLIBS')
 configopts['pyopt'] = distutils.sysconfig.get_config_var('OPT')
+source_obj = []
+for source_file in glob.iglob("*.c"):
+    source_obj.append(re.sub(r'(.+?)\.c', r'\1.o', source_file))
+configopts['deps'] = ' '.join(source_obj)
 targets = ''
-for arg in sys.argv[1:]:
-    targets += arg + ' '
+
+
+arg = sys.argv[1]
+targets += arg + ' '
 configopts['programs'] = targets
 print maketemplate % configopts
-for arg in sys.argv[1:]:
-    print "%s: %s.o\n\tgcc %s.o $(LIBS) $(PYLIB) -o %s" \
-            % (arg, arg, arg, arg)
-print "%s.o: %s.c\n\tgcc %s.c -c $(PYINC) $(OPTS)" \
-        % (arg, arg, arg)
+# for arg in sys.argv[1]:
+print "%s: $(DEPS)\n\tgcc $(DEPS) $(LIBS) $(PYLIB) -o %s" \
+        % (arg, arg)
+print "%.o: %.c\n\tgcc -o $@ $^ -c $(PYINC) $(OPTS)"
 print "clean:\n\trm -f $(PROGRAMS) *.o *.pyc core"
--- test_class.c
+++ test_class.c
@@ -1,111 +1,33 @@
-#include <Python.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <assert.h>
+#include "embedded_python_helper.h"
 
-struct array_string {
-	char **data;
-	size_t *nd;
-	size_t n;
-};
-
-void push_item_array_string(const char *item, const size_t ni) {
+void test_my_module()
+{
+	PyObject *my_obj = import_module("my_test");
+	int f = 0;
+	f = PyObject_HasAttrString(my_obj, "test");
+	fprintf(stderr, "%d has attr string test\n");
+	// assert(f == 6);
 	
+
+	PyObject *test = get_submodule(my_obj, "test");
+	PyObject *res = call_object(test, NULL);
+	assert(res);
+	CH_DECREF(test);
+	CH_DECREF(res);
 }
 
-void debug_pystring_list(PyObject *obj)
+void numpy_test()
 {
-	fprintf(stderr, "debug pylist of strings\n");
-	int n = PyList_Size(obj);
-	int i;
-	for (i = 0; i < n; ++i) {
-		PyObject *item = PyList_GetItem(obj, i);
-		Py_INCREF(item);
-		if (PyString_Check(item)) {
-			char *sps = PyString_AsString(item);
-			fprintf(stderr, "debug item %d %s\n", i, sps);
-		} else {
-			fprintf(stderr, "Item is not string\n");
-		}
-		Py_DECREF(item);
-	}
+	PyObject *np_obj = import_module("numpy");
+	assert(np_obj);
+	PyObject *obj = get_submodule(np_obj, "array");
+	assert(obj);
+	CH_DECREF(np_obj);
 }
 
-// append syspath to load custom modules path
-void append_syspath(const char *path)
+void example_test()
 {
-	// setting syspath
-	// could be used to add more modules path
-	PyObject *syspath = PySys_GetObject((char*)"path");
-	assert(syspath);
-	if (syspath) {
-		fprintf(stderr, "before set path\n");
-		PyObject *pypath = PyString_FromString(path);
-		assert(pypath);
-		PyList_Append(syspath, pypath);
-		debug_pystring_list(syspath);
-		Py_DECREF(syspath);
-	} else {
-		fprintf(stderr, "Cannot get syspath\n");
-	}
-}
-
-// wrapper function to load python module from python path
-PyObject *import_module(const char *module_name)
-{
-	PyObject *lmod = NULL;
-	lmod = PyImport_ImportModule(module_name);
-	if (!lmod) {
-		PyErr_Print();
-		fprintf(stderr, "Error loading module %s\n", module_name);
-		return NULL;
-	}
-	return lmod;
-}
-
-// get submodule part from the object
-// used for getting class out of module
-PyObject *get_submodule(PyObject *obj, const char *sub)
-{
-	PyObject *submod = NULL;
-	submod = PyObject_GetAttrString(obj, sub);
-	if (!submod) {
-		PyErr_Print();
-		fprintf(stderr, "Error getting submodule %s\n", sub);
-		return NULL;
-	}
-	return submod;
-}
-
-char **split_nested_submodule(const char *sub, const int ns) {
-	int i;
-	char *buffer = calloc(ns, sizeof(char));
-	// for (i = 0; i < ns; ++i) {
-	// }
-	PyObject *result;
-	return result;
-}
-
-PyObject *get_nested_submodule(PyObject *obj, const char *sub)
-{
-	PyObject *result;
-	return result;
-}
-
-// call object to run and return value
-// args must be a python tuple
-PyObject *call_object(PyObject *obj, PyObject *args)
-{
-	PyObject *result = NULL;
-	if (obj) {
-		result = PyEval_CallObject(obj, args);
-		if (!result) {
-			fprintf(stderr, "Error calling object\n");
-			return NULL;
-		}
-	}
-	return result;
+	// decrease all object ref to feed the garbage collector
 }
 
 int main(int argc, char *argv[])
@@ -117,33 +39,9 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	append_syspath(".");
-
-	PyObject *os_obj = import_module("os");
-	assert(os_obj);
-	PyObject *curdir = get_submodule(os_obj, "curdir");
-	assert(curdir);
-	// PyObject *pip_obj = import_module("numpy");
-	// assert(pip_obj);
-
-	// // assume that curdir is a string
-	// // check string
-	if (PyString_Check(curdir)) {
-		char *str = PyString_AsString(curdir);
-		fprintf(stderr, "debug string %s\n", str);
-	}
-
-
-	// prepare to finalize
-	// decrease all references
-	if (os_obj)
-		Py_DECREF(os_obj);
-	if (curdir)
-		Py_DECREF(curdir);
-	// if (pip_obj)
-	// 	Py_DECREF(pip_obj);
-
-	// decrease all object ref to feed the garbage collector
+	append_syspath("");
+	test_my_module();
+	fprintf(stderr, "Before Finalize\n");
 	Py_Finalize();
 	return 0;
 }
